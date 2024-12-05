2bfa7a6c78db1a5696405e04dfe65fdeeaaea77f
the-sleepingfox <ayushtripathi.india@gmail.com>
2024-12-05 12:59:55 +0530

    bug fixing

--- anime_app/uiurls.py
+++ anime_app/uiurls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import(home_page, login_view, logout_view, dashboard_view, register_view, search_view)
+from .views import(home_page, login_view, logout_view, dashboard_view, register_view)
 
 urlpatterns = [
     path('', home_page, name="home_page"),
@@ -7,5 +7,4 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('dashboard/', dashboard_view, name='dashboard'),
-    path('search/', search_view, name='search'),
 ]
