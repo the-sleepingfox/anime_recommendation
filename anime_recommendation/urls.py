0720f1bc6445400c09b236c7984133306733e080
the-sleepingfox <ayushtripathi.india@gmail.com>
2025-03-26 00:49:26 +0530

    adding swagger for api documentation

--- anime_recommendation/urls.py
+++ anime_recommendation/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from anime_app.views import home_page
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('', include('anime_app.uiurls') ),
     path('admin/', admin.site.urls),
     path('api/', include('anime_app.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
